a51b92a47a2ed4fb58f3d1996365192745be15cd
Justin Kuang <justin.kuang63@myhunter.cuny.edu>
2020-08-27 10:24:12 -0400

    Modify JS Functions and CSS
    Change selectNumberOfGrids to reset the grid so previous grid does not try to merge or squish together with the new grid. Also changed the div box widths to make the grid lines thinner and had the grid fit the viewport better

--- script.js
+++ script.js
@@ -20,46 +20,44 @@ function generateRandomIntFromInterval(min, max)
    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function generateRandomElementName(grid) 
+function generateRandomNumberedElementName(grid) 
 {
    let num = generateRandomIntFromInterval(1,100);
    grid += num.toString();
    return grid;
 }
 
-let gridContainer = document.querySelector('#grid-container'); 
 function createGrids(numberOfGrids) 
 {
+   let gridContainer = document.getElementById("grid-container")
    for (let boxes = 0; boxes < numberOfGrids; boxes++)
    {
-      //let boxName = generateRandomElementName("grid");
-      let anotherBox = document.createElement('div');
-      //anotherBox.id = boxName;
-      anotherBox.classList = "grid-container"
-      anotherBox.style.borderStyle = "solid";
-      //anotherBox.textContent = boxName;
-      anotherBox.style.fontSize = "10px"
-      gridContainer.appendChild(anotherBox);
+      let box = document.createElement('div');
+      box.classList = "box"
+      box.style.borderStyle = "solid";
+      box.style.borderWidth = "thin";
+      gridContainer.appendChild(box);
    }
 }
-//createGrids(10)
+
+createGrids(256)
 
 function selectNumberOfGrids(e) 
 {
    let gridContainer = document.getElementById('grid-container');
+   gridContainer.innerHTML = "";
    let numberOfGrids = document.getElementById('setSize').value;
-   let rowGridPixelSize = 940/numberOfGrids;
+   let rowGridPixelSize = 900/numberOfGrids;
    let totalNumberOfBoxes = Math.pow(numberOfGrids,2);
    gridContainer.style.setProperty('grid-template-columns', `repeat(${numberOfGrids}, 1fr)`);
    gridContainer.style.setProperty('grid-template-rows', `repeat(${numberOfGrids}, ${rowGridPixelSize}px)`);
    createGrids(totalNumberOfBoxes);
 }
 
-/* function clearGrid() 
+function clearGrid() 
 {
-   let gridContainer = document.querySelectorAll('#grid-container');
-   gridContainer.
-} */
+
+} 
 
 document.querySelector('#setSizeButton').addEventListener('click', selectNumberOfGrids);
 
